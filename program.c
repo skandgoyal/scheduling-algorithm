1bc860b746f1e7e25e06e850527dfcadb1f97642
Skand Goyal <theskandgoyal@gmail.com>
2020-03-30 04:16:08 +0530

    Create program.c

---
+++ program.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("\t\t\t----------------------- Scheduling -----------------------\n\n\n\n");
+  long int n,i=0,j=0;
+  printf("Enter Number of Processes : ");
+  scanf("%ld",&n );
+  double priority[n],avg_waiting,avg_turnaround,burstTime[n],arrivalTime[n],waitingTime[n],turnaroundTime[n], process[n], temp, completionTime[n],min,sum=0,sum2=0,wait_final, turnaround_final, wait_avg, turnaround_avg;
+  for(i=0;i<n;i++)                                      //complexity = n
+  {
+    printf("\nEnter Burst Time for Process [%d] : ", i+1 );
+    scanf("%lf", &burstTime[i]);
+    printf("Enter Arrival Time for Process [%d] : ", i+1 );
+    scanf("%lf", &arrivalTime[i] );
+    process[i]=i+1;
+  }
+
+  printf("\n\n\t\t\t -------------- Entered Values are --------------\n\n");
+  printf("\t\t\t---------------------------------------\n");
+  printf("\t\t\t| Process | Arrival Time | Burst Time |\n");
+  printf("\t\t\t---------------------------------------\n");
+  for(i=0;i<n;i++)                                        //complexity = n
+  {
+    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |\n",process[i],arrivalTime[i],burstTime[i]);
+  }
+    printf("\t\t\t---------------------------------------\n");
+
+
+  printf("\n\n\t\t\t-------- Sorting Processes according to Arrivaltime --------\n");
+
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n;j++)                                                      //complexity = n^2
+    {
+      if(arrivalTime[i]<arrivalTime[j])
+      {
+
+        temp = burstTime[j];
+        burstTime[j] = burstTime[i];
+        burstTime [i] = temp;
+
+	      temp = process[j];
+        process[j] = process[i];
+        process[i] = temp;
+
+	      temp = arrivalTime[j];
+        arrivalTime[j] = arrivalTime[i];
+        arrivalTime[i] = temp;
+
+      }
+    }
+  }
+  printf("\n\n\t\t\t -------------- Now Values are --------------\n\n");
+  printf("\t\t\t---------------------------------------\n");
+  printf("\t\t\t| Process | Arrival Time | Burst Time |\n");
+  printf("\t\t\t---------------------------------------\n");
+  for(i=0;i<n;i++)                                                  //complexity = n
+  {
+    printf("\t\t\t|  P[%0.0lf]   |       %0.0lf      |     %0.0lf      |\n",process[i],arrivalTime[i],burstTime[i]);
+  }
+    printf("\t\t\t---------------------------------------\n");
+
